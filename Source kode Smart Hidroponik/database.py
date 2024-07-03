31d76d0f0c09d6ada88930af038513b90ac7430d
Yeahthu <122600104+Yeahthu@users.noreply.github.com>
2024-07-03 12:32:16 +0000

    Update database.py
    tidak pakai mqtt

--- Source kode Smart Hidroponik/database.py
+++ Source kode Smart Hidroponik/database.py
@@ -1,39 +1,5 @@
-# from flask import Flask, request, jsonify # Memanggil flask untuk membuat local server , request untuk  dan jsonify
-# import paho.mqtt.client as mq # Untuk mengaksees MQTT 
-# from pymongo import MongoClient as mongo # Untuk mengakses database menggunakan mongo 
-# from pymongo.errors import ConnectionFailure 
-
-# web = Flask(__name__) #untuk inisialisasi flask 
-
-# # Koneksi ke MongoDB
-# url_database = "mongodb+srv://SmartHidroponik:MERA_X@smarthidroponik.hdetbis.mongodb.net/?retryWrites=true&w=majority&appName=SmartHidroponik"
-
-# @web.route('/cek_url')
-# def cek_url():
-#         # Inisialisasi client MongoDB
-#         client = mongo(url_database)
-        
-#         # Pilih database
-#         db = client.Smart_Hidroponik
-        
-#         # Pilih koleksi
-#         collection = db.Sensor_TDS
-        
-#         # Coba lakukan operasi sederhana untuk memastikan koneksi
-#         sample_doc = collection.find_one()
-        
-#         if sample_doc:
-#             return "Koneksi berhasil ", 200
-#         elif ConnectionFailure:
-#              return "Koneksi ke MongoDB gak berhasil", 500
-#         else:
-#             return "oops data kamu kosong"
-
-# if __name__ == '__main__':
-#     web.run(debug=True)
 
 from flask import Flask, request, jsonify # Memanggil flask untuk membuat local server, request untuk dan jsonify
-import paho.mqtt.client as mq # Untuk mengakses MQTT 
 from pymongo import MongoClient as mongo # Untuk mengakses database menggunakan mongo 
 from pymongo.errors import ConnectionFailure 
 
